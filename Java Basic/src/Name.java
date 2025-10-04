8783f727696fdc7a6a0efb5e889ace5e4e48cee9
HMGChamidu <chamindusandeepa339@gmail.com>
2025-10-04 19:47:33 +0530

    creat name filee

--- Java Basic/src/Name.java
+++ Java Basic/src/Name.java
@@ -3,5 +3,6 @@ import groovy.json.JsonOutput;
 public class Name {
     public static void main(String[] args) {
         System.out.println("xjnsjkjcskcdmc");
+        System.out.println();
     }
 }
