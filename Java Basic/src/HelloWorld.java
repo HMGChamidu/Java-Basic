18e0a50c8aab02f7ac2445466c00b4d2ed48652a
HMGChamidu <chamindusandeepa339@gmail.com>
2025-10-05 09:14:14 +0530

    Input & Output in java

--- Java Basic/src/HelloWorld.java
+++ Java Basic/src/HelloWorld.java
@@ -1,5 +1,15 @@
+import  java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! It's me Chamidu");
+       double studentGPA= 3.45;
+       String studentFirstName= "Chamidu";
+       String studentSecondName="Sandeepa";
+       System.out.println(studentFirstName+" "+studentSecondName+ " has a GPA of "+studentGPA);
+        System.out.println("What do you want to update it to?");
+        Scanner input = new Scanner(System.in);
+
+        studentGPA=input.nextDouble();
+        System.out.println(studentFirstName+" "+studentSecondName+" now has the GPA of "+ studentGPA);
     }
 }
