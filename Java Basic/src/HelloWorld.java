1f7d3a275ba81bb2c0ef96175c4a93a261ed7379
HMGChamidu <chamindusandeepa339@gmail.com>
2025-10-06 11:01:42 +0530

    function in java

--- Java Basic/src/HelloWorld.java
+++ Java Basic/src/HelloWorld.java
@@ -1,22 +1,17 @@
 import  java.util.Scanner;
 
 public class HelloWorld {
+    public static void announceDeveloperTeaTime(){
+        System.out.println("Waiting for developer tea time...");
+        System.out.println("Type in a random word and press Enter to start developer tea time");
+        Scanner input=new Scanner(System.in);
+        input.next();
+        System.out.println("It's a developer tea time! ");
+    }
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        boolean isOnRepeat= true;
 
-        while(isOnRepeat){
-            System.out.println("Playing current song");
-            System.out.println("Woud you like to take this song off of repeat? if so, answer yes ");
-            String answer= input.next();
-
-            if(answer.equals("yes")){
-                isOnRepeat=false;
-            }
-        }
-        System.out.println("Playing next song");
-
+    }
 
 
     }
-}
+
