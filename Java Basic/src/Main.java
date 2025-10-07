64bb93ea004271d9c98ac9f2de221776eacb6db6
HMGChamidu <chamindusandeepa339@gmail.com>
2025-10-07 15:06:44 +0530

    Find area using instance methods

--- Java Basic/src/Main.java
+++ Java Basic/src/Main.java
@@ -1,3 +1,20 @@
-static void main(String[] args) {
-    println "Hello world!"
+public class Main {
+    public static void main(String[] args) {
+     Triangle triangleA=new Triangle(15,8,15,8,17);
+     Triangle triangleB = new Triangle(3,2.598,3,3,3);
+
+     double triangleArea = triangleA.findArea();
+
+        System.out.println(triangleArea);
+
+
+    }
+
+
 }
+
+
+
+
+
+
