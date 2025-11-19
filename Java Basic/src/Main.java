225fe52e1ceb3816e0502b55778322dc92d9e55b
HMGChamidu <chamindusandeepa339@gmail.com>
2025-11-19 10:25:39 +0530

    Scope Local

--- Java Basic/src/Main.java
+++ Java Basic/src/Main.java
@@ -1,28 +1,16 @@
-public class Main {
+
+
+public class  Main {
+
     public static void main(String[] args) {
-     Triangle triangleA=new Triangle(15,8,15,8,17);
-     Triangle triangleB = new Triangle(3,2.598,3,3,3);
-
-     double triangleArea = triangleA.findArea();
-
-        System.out.println(triangleArea);
+             int x =1; //Local
 
 
-        double triangleBArea=triangleB.findArea();
-        System.out.println(triangleBArea);
-
-        System.out.println(triangleA.sideLenThree);
-        System.out.println(triangleA.base);
-
-        System.out.println(Triangle.numOfSides);
-
+        doSomething();
     }
 
-
+    static void doSomething(){
+        int x =2; //LOCAL
+        System.out.println(x);
+    }
 }
-
-
-
-
-
-
